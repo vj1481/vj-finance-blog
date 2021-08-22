6c43657f9f023c573851509c5647159346cddc3d
vijay <vsgundlapalli@teksavvy.ca>
2021-08-22 01:59:40 -0400

    tools draft1

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Loan Amortization Calculator',
+    description: `The most important calculation of any loan in personal finance is the interest rate and it's impact on amortization. Use this tools to chart the interest you might pay on a given loan by plugging your numbers.`,
+    imgSrc: '/static/images/undraw_Calculator.svg',
+    href: '/tools/loan-calculator',
   },
 ]
 
---
+++ pages/tools/loan-calculator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Calculator } from '../../components/amortization-calculator/Calculator'
+
+function LoanCalculator() {
+  return (
+    <div>
+      <Calculator />
+      {/* <CalculatorHookForm /> */}
+    </div>
+  )
+}
+
+export default LoanCalculator
