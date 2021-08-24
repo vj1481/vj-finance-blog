fbdd9e922e5cad04c7a23f259c4bc6e242a4bdb4
vijaysudheerwebmails@gmail.com <vijaysudheerwebmails@gmail.com>
2021-08-24 02:19:45 -0400

    prettier

--- components/amortization-calculator/CalculatorControls.js
+++ components/amortization-calculator/CalculatorControls.js
@@ -8,82 +8,82 @@ const localeOptions = {
 }
 
 export const CalculatorControls = (props) => {
-  return (<>
-    <div className="flex  justify-between">
-      <div className=" text-left  flex-grow font-semibold space-y-2">
-        <span className="bottom-2">
-          {parseInt(props.purchasingHousePrice).toLocaleString('en-IN', localeOptions)}
-        </span>
-             
-        <div ><input 
-          type="range"
-          className=" w-96"
-          id="purchasingHousePrice"
-          min="50000"
-          max="7500000"
-          step="1000"
-          value={props.purchasingHousePrice}
-          onChange={(event) => props.setPurchasingHousePrice(event.target.value)}
-        />
-  
+  return (
+    <>
+      <div className="flex  justify-between">
+        <div className=" text-left  flex-grow font-semibold space-y-2">
+          <span className="bottom-2">
+            {parseInt(props.purchasingHousePrice).toLocaleString('en-IN', localeOptions)}
+          </span>
+
+          <div>
+            <input
+              type="range"
+              className=" w-96"
+              id="purchasingHousePrice"
+              min="50000"
+              max="7500000"
+              step="1000"
+              value={props.purchasingHousePrice}
+              onChange={(event) => props.setPurchasingHousePrice(event.target.value)}
+            />
+          </div>
+
+          <label className=" italic font-light">Purchasing House Price</label>
         </div>
-        
-        <label className=" italic font-light" >
-          Purchasing House Price
-        </label>
+        <div className="grid__item">
+          <span className="grid__item--header">
+            {parseInt(props.depositAmount).toLocaleString('en-IN', localeOptions)}
+          </span>
+          <input
+            type="range"
+            className="grid__item--range-slider"
+            id="depositAmount"
+            min="1000"
+            max="150000"
+            step="1000"
+            value={props.depositAmount}
+            onChange={(event) => props.setDepositAmount(event.target.value)}
+          />
+          <label className="grid__item--label" htmlFor="points">
+            Deposit
+          </label>
+        </div>
       </div>
-      <div className="grid__item">
-        <span className="grid__item--header">
-          {parseInt(props.depositAmount).toLocaleString('en-IN', localeOptions)}
-        </span>
-        <input
-          type="range"
-          className="grid__item--range-slider"
-          id="depositAmount"
-          min="1000"
-          max="150000"
-          step="1000"
-          value={props.depositAmount}
-          onChange={(event) => props.setDepositAmount(event.target.value)}
-        />
-        <label className="grid__item--label" htmlFor="points">
-          Deposit
-        </label>
+      <div className="flex just">
+        <div className="grid__item">
+          <span className="grid__item--header">{props.mortgageTerm} Years</span>
+          <input
+            type="range"
+            className="grid__item--range-slider"
+            id="mortgageTerm"
+            min="5"
+            max="35"
+            step="1"
+            value={props.mortgageTerm}
+            onChange={(event) => props.setMortgageTerm(event.target.value)}
+          />
+          <label className="grid__item--label" htmlFor="mortgageTerm">
+            Mortgage Term
+          </label>
+        </div>
+        <div className="grid__item">
+          <span className="grid__item--header">{props.interestRate}%</span>
+          <input
+            type="range"
+            className="grid__item--range-slider"
+            id="interestRate"
+            min="0.1"
+            max="8"
+            step="0.1"
+            value={props.interestRate}
+            onChange={(event) => props.setInterestRate(event.target.value)}
+          />
+          <label className="grid__item--label" htmlFor="interestRate">
+            Interest Rate
+          </label>
+        </div>
       </div>
-</div>
-<div className="flex just">
-      <div className="grid__item">
-        <span className="grid__item--header">{props.mortgageTerm} Years</span>
-        <input
-          type="range"
-          className="grid__item--range-slider"
-          id="mortgageTerm"
-          min="5"
-          max="35"
-          step="1"
-          value={props.mortgageTerm}
-          onChange={(event) => props.setMortgageTerm(event.target.value)}
-        />
-        <label className="grid__item--label" htmlFor="mortgageTerm">
-          Mortgage Term
-        </label>
-      </div>
-      <div className="grid__item">
-        <span className="grid__item--header">{props.interestRate}%</span>
-        <input
-          type="range"
-          className="grid__item--range-slider"
-          id="interestRate"
-          min="0.1"
-          max="8"
-          step="0.1"
-          value={props.interestRate}
-          onChange={(event) => props.setInterestRate(event.target.value)}
-        />
-        <label className="grid__item--label" htmlFor="interestRate">
-          Interest Rate
-        </label>
-      </div>
-    </div>
-</>  )
+    </>
+  )
 }
