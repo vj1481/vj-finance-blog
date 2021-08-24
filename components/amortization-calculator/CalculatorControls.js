10617286530b01c5a17a7e4c165d1e20e2338f6d
vijaysudheerwebmails@gmail.com <vijaysudheerwebmails@gmail.com>
2021-08-24 03:09:40 -0400

    c

--- components/amortization-calculator/CalculatorControls.js
+++ components/amortization-calculator/CalculatorControls.js
@@ -15,7 +15,7 @@ export const CalculatorControls = (props) => {
           <span className="bottom-2">
             {parseInt(0).toLocaleString('en-IN', localeOptions).replace(/\d/g, '').trim()}
           </span>
-           <input
+          <input
             className="pl-2 "
             min="50000"
             max="7500000"
@@ -40,21 +40,20 @@ export const CalculatorControls = (props) => {
           <label htmlFor="purchasingHousePrice" className=" italic font-light">
             Purchasing House Price
           </label>
-         
         </div>
         <div className="text-left  flex-grow font-semibold space-y-2">
-        <span className="bottom-2">
+          <span className="bottom-2">
             {parseInt(0).toLocaleString('en-IN', localeOptions).replace(/\d/g, '').trim()}
           </span>
-           <input
+          <input
             className="pl-2 "
-              id="depositAmount"
-              min="1000"
-              max="150000"
-              step="1000"
-              value={props.depositAmount}
-              onChange={(event) => props.setDepositAmount(event.target.value)}
-            />
+            id="depositAmount"
+            min="1000"
+            max="150000"
+            step="1000"
+            value={props.depositAmount}
+            onChange={(event) => props.setDepositAmount(event.target.value)}
+          />
           <div>
             <input
               type="range"
@@ -76,16 +75,17 @@ export const CalculatorControls = (props) => {
         <div className="text-left  flex-grow font-semibold space-y-2">
           <span className="bottom-2">{props.mortgageTerm} Years</span>
           <div>
-          <input
-            type="range"
-            className=" w-80"
-            id="mortgageTerm"
-            min="5"
-            max="35"
-            step="1"
-            value={props.mortgageTerm}
-            onChange={(event) => props.setMortgageTerm(event.target.value)}
-          /></div>
+            <input
+              type="range"
+              className=" w-80"
+              id="mortgageTerm"
+              min="5"
+              max="35"
+              step="1"
+              value={props.mortgageTerm}
+              onChange={(event) => props.setMortgageTerm(event.target.value)}
+            />
+          </div>
           <label className="italic font-light" htmlFor="mortgageTerm">
             Mortgage Term
           </label>
@@ -93,16 +93,17 @@ export const CalculatorControls = (props) => {
         <div className="text-left  flex-grow font-semibold space-y-2">
           <span className="bottom-2">{props.interestRate}%</span>
           <div>
-          <input
-            type="range"
-            className="w-80"
-            id="interestRate"
-            min="0.1"
-            max="8"
-            step="0.1"
-            value={props.interestRate}
-            onChange={(event) => props.setInterestRate(event.target.value)}
-          /></div>
+            <input
+              type="range"
+              className="w-80"
+              id="interestRate"
+              min="0.1"
+              max="8"
+              step="0.1"
+              value={props.interestRate}
+              onChange={(event) => props.setInterestRate(event.target.value)}
+            />
+          </div>
           <label className="italic font-light" htmlFor="interestRate">
             Interest Rate
           </label>
--- components/amortization-calculator/PaymentTable.js
+++ components/amortization-calculator/PaymentTable.js
@@ -10,14 +10,12 @@ export const PaymentTable = (props) => {
   let paymentMonthsArr = []
   for (let i = 0; i < props.yearlyPayments.length; i++) {
     paymentMonthsArr.push(
-      <tr  key={props.yearlyPayments[i].year}>
+      <tr key={props.yearlyPayments[i].year}>
         <td>{props.yearlyPayments[i].year}</td>
         <td>{props.yearlyPayments[i].interestPaid.toLocaleString('en-IN', localeOptions)}</td>
-        <td >
-          {props.yearlyPayments[i].interestPaidToDate.toLocaleString('en-IN', localeOptions)}
-        </td>
+        <td>{props.yearlyPayments[i].interestPaidToDate.toLocaleString('en-IN', localeOptions)}</td>
         <td>{props.yearlyPayments[i].principalRepaid.toLocaleString('en-IN', localeOptions)}</td>
-        <td >
+        <td>
           {props.yearlyPayments[i].principalRepaidToDate.toLocaleString('en-IN', localeOptions)}
         </td>
         <td>{props.yearlyPayments[i].outstandingBalance.toLocaleString('en-IN', localeOptions)}</td>
@@ -28,19 +26,22 @@ export const PaymentTable = (props) => {
   return (
     <section class="container mx-auto p-6 font-mono">
       <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg pl-4  bg-purple-100">
-      <div class="w-full overflow-x-auto">
-    <table className="table-auto w-full">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Year Interest Paid</th>
-          <th >Total Interest Paid</th>
-          <th>Year Principal Repaid</th>
-          <th >Total Principal Repaid</th>
-          <th>Mortgage Balance</th>
-        </tr>
-      </thead>
-      <tbody >{paymentMonthsArr}</tbody>
-    </table></div></div></section>
+        <div class="w-full overflow-x-auto">
+          <table className="table-auto w-full">
+            <thead>
+              <tr>
+                <th>Year</th>
+                <th>Year Interest Paid</th>
+                <th>Total Interest Paid</th>
+                <th>Year Principal Repaid</th>
+                <th>Total Principal Repaid</th>
+                <th>Mortgage Balance</th>
+              </tr>
+            </thead>
+            <tbody>{paymentMonthsArr}</tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   )
 }
