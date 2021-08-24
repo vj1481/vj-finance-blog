7c41f176268dee0d714b01ee012882f68823f59f
vijaysudheerwebmails@gmail.com <vijaysudheerwebmails@gmail.com>
2021-08-24 01:33:58 -0400

    changes

--- components/amortization-calculator/HeaderFigures.js
+++ components/amortization-calculator/HeaderFigures.js
@@ -8,45 +8,47 @@ const localeOptions = {
 }
 
 export const HeaderFigures = (props) => {
-  return (
-    <div className="flex flex-row">
-      <div className="text-left space-y-3 mr-20">
+  return (<div className=" space-y-12">
+    <div className="flex justify-between ">
+      <div className="text-left font-light  space-y-3">
         <span className="font-bold bottom-2">
           {parseInt(props.amountToBorrow).toLocaleString('en-IN', localeOptions)}
         </span>
         <div className="italic"> Amount To Borrow</div>
       </div>
-      <div className="text-right space-y-3 mr-20">
+      <div className=" text-center font-light space-y-3">
         <span className="font-bold bottom-4">
           {parseInt(props.monthlyPayment).toLocaleString('en-IN', localeOptions)}
         </span>
         <div className="italic">Monthly Payment</div>
       </div>
-      ;
-      <div className="text-right space-y-3 mr-20">
+      
+      <div className="text-right font-light space-y-3">
         <span className="font-bold bottom-2">
           {parseInt(props.totalRepaid).toLocaleString('en-IN', localeOptions)}
         </span>
         <div className="italic">Total Repaid</div>
       </div>
-      <div className="text-right space-y-3 mr-20 ">
+      </div>
+    <div className="flex justify-between">
+    <div className="text-left space-y-3  ">
         <span className="font-bold text-red-500  bottom-2">
           {parseInt(props.totalInterestPaid).toLocaleString('en-IN', localeOptions)}
         </span>
         <div className="italic">Total Interest Paid</div>
       </div>
-      <div className="text-right space-y-3 mr-20">
+      <div className="text-center space-y-3 ">
         <span className="font-bold text-green-600 bottom-2">
           {((props.depositAmount / props.purchasingHousePrice) * 100).toFixed(1)}%
         </span>
         <div className="italic">Deposit</div>
       </div>
-      <div className="text-right space-y-3 mr-20">
+      <div className="text-right space-y-3 ">
         <span className="font-bold bottom-2">
           {((props.amountToBorrow / props.purchasingHousePrice) * 100).toFixed(1)}%
         </span>
         <div className="italic">Loan To Value</div>
       </div>
     </div>
-  )
+</div>  )
 }
